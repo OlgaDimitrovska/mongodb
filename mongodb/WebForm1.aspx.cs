5af883a6ef72c689c9972ffd07e2af89742b257e
olgad <olgad@OLGAD>
2018-02-07 10:31:48 +0100

    sum go komentirala str1

--- mongodb/WebForm1.aspx.cs
+++ mongodb/WebForm1.aspx.cs
@@ -26,7 +26,7 @@ namespace mongodb
             IMongoCollection<Info> valuti = database.GetCollection<Info>("Valuti");
             valuti.Find(_ => true).ToList().ForEach(valuta =>
             {
-                str1 = str1 + valuta.Date + " "+ valuta.Valuta + " " + valuta.Kupoven + " " + valuta.Sreden + " " + valuta.Prodazen + "</br>";
+                //str1 = str1 + valuta.Date + " "+ valuta.Valuta + " " + valuta.Kupoven + " " + valuta.Sreden + " " + valuta.Prodazen + "</br>";
                 if(Convert.ToDateTime(valuta.Date)==DateTime.Today)
                 lista.Add(valuta);
             });
