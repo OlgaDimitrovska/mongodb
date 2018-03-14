616fcfe90aa3122069ded71e0c56ced80a67aa64
olgad <olgad@OLGAD>
2018-03-14 16:07:28 +0100

    dodaden query i napravena tabela

--- mongodb/WebForm1.aspx.cs
+++ mongodb/WebForm1.aspx.cs
@@ -9,6 +9,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using mongodb.Models;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Builders;
 
 namespace mongodb
 {
@@ -23,34 +25,46 @@ namespace mongodb
                 MainView.ActiveViewIndex = 0;
             }
         }
-
+        //public void postaviRegionalOptions()
+        //{
+        //    var appCulture = new System.Globalization.CultureInfo("mk-MK");
+        //    appCulture.DateTimeFormat.ShortDatePattern = "dd.MM.yyyy";
+        //    appCulture.DateTimeFormat.LongDatePattern = "dd.MM.yyyy";
+        //    appCulture.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
+        //    appCulture.NumberFormat.NumberDecimalSeparator = ",";
+        //    appCulture.NumberFormat.NumberGroupSeparator = ".";
+        //    appCulture.NumberFormat.CurrencyDecimalSeparator = ",";
+        //    appCulture.NumberFormat.CurrencyGroupSeparator = ".";
+        //}
         protected void Button1_Click(object sender, EventArgs e)
         {
             DataTable tabela = new DataTable();
             var server = new MongoClient(MongoUrl.Create("mongodb://localhost:27017"));
             IMongoDatabase database = server.GetDatabase("prvaDB");
-            IMongoCollection<Info> valuti = database.GetCollection<Info>("Valuti");
+            var collection = database.GetCollection<Info>("Valuti");
 
-            //lista = valuti.Find(_ => true).ToList();
+            
             DataColumn dc1 = new DataColumn("Датум");
+            DataColumn dc5 = new DataColumn("Назив на Банка");
             DataColumn dc2 = new DataColumn("Валута");
             DataColumn dc3 = new DataColumn("Куповен");
             DataColumn dc4 = new DataColumn("Продажен");
-            DataColumn dc5 = new DataColumn("Назив на Банка");
+            
             tabela.Columns.Add(dc1);
+            tabela.Columns.Add(dc5);
             tabela.Columns.Add(dc2);
             tabela.Columns.Add(dc3);
             tabela.Columns.Add(dc4);
-            tabela.Columns.Add(dc5);
-            valuti.Find(_ => true).ToList().ForEach(valuta =>
+            
+            
+            var query = from p in collection.AsQueryable<Info>() where p.Valuta=="eur" && p.Date==Convert.ToString(DateTime.Now.ToShortDateString()) select p;
+            foreach (var t in query)
             {
-                //str1 = str1 + valuta.Date + " "+ valuta.Valuta + " " + valuta.Kupoven + " " + valuta.Sreden + " " + valuta.Prodazen + "</br>";
-                if (Convert.ToString(valuta.Date) == "14.03.2018")
-                    tabela.Rows.Add(valuta.Date,valuta.Valuta,valuta.Kupoven,valuta.Prodazen,valuta.Banka.Name);
-                   
-            });
+                tabela.Rows.Add(t.Date,t.Banka.Name,t.Valuta,t.Kupoven,t.Prodazen); 
+            }
+           
 
-            //Label1.Text = str1;
+            
             GridView1.DataSource = tabela;
             GridView1.DataBind();
 
@@ -59,6 +73,7 @@ namespace mongodb
             //Chart1.Series[0].YValueMembers = "Sreden";
             //Chart1.DataBind();
         }
+  
 
         protected void Tab1_Click(object sender, EventArgs e)
         {
